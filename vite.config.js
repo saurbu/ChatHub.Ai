7acd15d0e10c8ae3defca429336bba893f50001f
saurbu <ss1742@dseu.ac.in>
2026-06-04 02:45:22 +0530

    fix: hard modification to vite config file

--- vite.config.js
+++ vite.config.js
@@ -6,3 +6,14 @@ export default defineConfig({
   plugins: [react()],
   base: '/ChatHub.Ai/',
 })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/ChatHub.Ai/',
+})
+
+// Force update check v1
